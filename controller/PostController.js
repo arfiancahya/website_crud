e8d6269fbf0eb3afe78279243f9f982f4eafd11a
arfiancahya <arfian.dwisetya@gmail.com>
2020-08-20 15:48:09 +0700

    Menambahkan Create

--- controller/PostController.js
+++ controller/PostController.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const model = require("../models");
+const Po = model.posts;
+const Op = model.Sequelize.Op;
 
 const getAllPost = async (req, res) => {
-    const Post = await model.Post.findAll({});
+    const Post = await Po.findAll({});
     res.status(200).send({
         status: 200,
         message: "Berhasil get data post",
@@ -10,6 +12,41 @@ const getAllPost = async (req, res) => {
     });
 };
 
+const createPost = async (req, res) => {
+    try {
+
+        if (!req.body.title) {
+            res.status(400).send({
+                message: "Content can not be empty!"
+            });
+            return;
+        }
+
+        const post = {
+            title: req.body.title,
+            description: req.body.description,
+            published: req.body.published ? req.body.published : false
+        };
+
+        const Post = await Po.create({
+            post
+        });
+
+        res.status(200).send({
+            status: 200,
+            message: "Post berhasil dibuat",
+            data: Post
+        });
+    } catch (error) {
+        res.status(500).send({
+            status: 500,
+            message: "Error membuat post"
+        });
+    }
+
+};
+
 module.exports = {
-    getAllPost
+    getAllPost,
+    createPost
 };
