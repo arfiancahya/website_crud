59127cbb963c82fc29366b07afe82768490a52f7
arfiancahya <arfian.dwisetya@gmail.com>
2020-08-20 12:41:46 +0700

    Menambahkan Server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {
+    config
+} = require("dotenv/types");
+const app = express();
+require("dotenv") config();
+
+const port = process.env.PORT_APP || 5001;
+app.listen(port, () => {
+    console.log("Server using on port ${port}");
+});
