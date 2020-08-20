8016f5ef4ec7dedaf93019a5a39fe2a8a18e7e8e
arfiancahya <arfian.dwisetya@gmail.com>
2020-08-20 13:45:01 +0700

    menambahkan body-parser

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const app = express();
 const routes = require("./routes");
 require("dotenv").config();
 
+//databasenya dan untuk memanggil file route, controller, dan models
 app.use("/api/", routes);
+
+//body parse
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
 const port = process.env.PORT_APP || 5001;
 app.listen(port, () => {
     console.log(`Server using on port ${port}`);
