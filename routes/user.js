b62c0ca1433ec9250f31731a8308798833cbb00e
arfiancahya <arfian.dwisetya@gmail.com>
2020-08-21 21:45:54 +0700

    Menambahkan Routes Middleware

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controller/UserController");
 const validasi = require("../validation");
+const middleware = require("../middleware/middleware");
 
 router.post("/daftar", validasi.validationDaftar, validasi.runValidation, UserController.daftarUser);
-router.post("/login", UserController.loginUser);
+router.post("/login", validasi.validationLogin, validasi.runValidation, UserController.loginUser);
+router.get("/user", middleware, UserController.getSingleUser);
 
 module.exports = router;
