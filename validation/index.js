7d59273e487ba36a8d2c3f079b3245313af4a236
arfiancahya <arfian.dwisetya@gmail.com>
2020-08-21 21:11:03 +0700

    Menambahkan Validation

---
+++ validation/index.js
@@ -0,0 +1,33 @@
+const {
+    check,
+    validationResult
+} = require("express-validator");
+
+const runValidation = (
+    req,
+    res,
+    next
+) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(404).send({
+            status: false,
+            message: errors.array()[0].msg
+        });
+    }
+    next()
+};
+
+const validationDaftar = [
+    check("username", "Username tidak boleh kosong").notEmpty(),
+    check("email", "Email tak boleh koskng").notEmpty().matches(/.+\@.+\..+/).withMessage("email harus menggunakan @"),
+    check("password", "Password harus diisi").notEmpty().isLength({
+        min: 6
+    }).withMessage("Password harus 6 character")
+]
+
+
+module.exports = {
+    runValidation,
+    validationDaftar
+}
