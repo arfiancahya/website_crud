61fb52e21f14cfa6b787e616bc2f46aa534a0148
arfiancahya <arfian.dwisetya@gmail.com>
2020-08-21 19:46:28 +0700

    Routes Login

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const UserController = require("../controller/UserController");
 
 router.post("/daftar", UserController.daftarUser);
+router.post("/login", UserController.loginUser);
 
 module.exports = router;
