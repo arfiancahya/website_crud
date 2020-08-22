2f8a9c239318ac9655b47e620e101c01e709ed14
arfiancahya <arfian.dwisetya@gmail.com>
2020-08-22 10:25:39 +0700

    Menambahkan Routes Forget Password

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,6 @@ const middleware = require("../middleware/middleware");
 router.post("/daftar", validasi.validationDaftar, validasi.runValidation, UserController.daftarUser);
 router.post("/login", validasi.validationLogin, validasi.runValidation, UserController.loginUser);
 router.get("/user", middleware, UserController.getSingleUser);
-router.put("/forger", UserController.forgetPassword);
+router.put("/forget", UserController.forgetPassword);
 
 module.exports = router;
