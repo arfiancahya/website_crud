c18ff926dfcdd3febdbeebed89181531e07cd243
arfiancahya <arfian.dwisetya@gmail.com>
2020-09-18 18:29:14 +0700

    Membenarkan upload file

--- routes/file.js
+++ routes/file.js
@@ -8,5 +8,6 @@ const upload = require('../config/multer.config');
 
 router.post('/upload', upload.single('uploadfile'), fileWorker.uploadFile); 
 router.get('/list', fileWorker.getFile);
+router.get('/donwload/:id', fileWorker.downloadFile);
 
 module.exports = router;
