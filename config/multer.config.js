7618ea8cc58bf91bf96f3e911bad746f680991f4
arfiancahya <arfian.dwisetya@gmail.com>
2020-09-18 18:28:55 +0700

    Membenarkan upload file

--- config/multer.config.js
+++ config/multer.config.js
@@ -1,18 +1,20 @@
 const multer = require('multer');
-const path  = require("path");
+const path = require("path");
 const fs = require("fs");
 
 const storage = multer.diskStorage({
-    destination: './client/public/uploads',
-    filename: function(req, file, callback) {
-		console.log(file)
-		callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-	}
+    destination: (req, file, callback) => { 
+        callback(null, __basedir + "/client/src/resources/static/assets/uploads/")
+    },
+    filename: function (req, file, callback) {
+        console.log(file)
+        callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
 });
 
 const upload = multer({
     storage: storage,
-    limits:{fileSize: 1000000}
+    limits: { fileSize: 1000000 }
 });
 
 module.exports = upload;
